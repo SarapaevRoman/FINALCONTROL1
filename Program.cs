636b4eefaaa0d20ace20a8462da9c6a6a806b91c
Roman <romansp007@mail.ru>
2023-12-14 22:59:11 +0300

    Writing code, creating a method RowFiltering, to filter strings

--- Program.cs
+++ Program.cs
@@ -1,1 +1,11 @@
-﻿/*
+﻿Console.Clear();
+Console.WriteLine("Введите строки через пробел: ");
+string row = Console.ReadLine();
+
+string[] array = row.Split(" ");
+
+string[] RowFiltering(string [] array )
+{
+    
+}
+
