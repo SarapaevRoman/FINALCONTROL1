cac5b37d6aa0decae0a497a3d5abbd15c5e98cd8
Roman <romansp007@mail.ru>
2023-12-14 23:56:16 +0300

    solution description

--- Program.cs
+++ Program.cs
@@ -1,24 +1,28 @@
 ﻿Console.Clear();
-Console.WriteLine("Введите строки через пробел: ");
+Console.WriteLine("Ведите строки через пробел:");
 string row = Console.ReadLine();
 
 string[] array = row.Split(" ");
 
-string[] RowFiltering(string [] array )
+
+string [] RowFiltering( string [] array)
 {
     string[] arr = new string[array.Length];
     int count = 0;
-
-    for( int i = 0; i < array.Length; i++)
+    for (int i = 0; i < array.Length; i++)
     {
         string input = array[i];
-
         if (input.Length <= 3)
         {
             arr[count] = input;
             count++;
-        }
+        }     
     }
     return arr;
 }
+
+
+
+
+
 
