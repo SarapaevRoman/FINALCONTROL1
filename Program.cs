1618be3b1f01a6296d906bb7f8655033d6dae2d9
Roman <romansp007@mail.ru>
2023-12-14 23:07:56 +0300

    Method for filtering strings

--- Program.cs
+++ Program.cs
@@ -6,6 +6,19 @@ string[] array = row.Split(" ");
 
 string[] RowFiltering(string [] array )
 {
-    
+    string[] arr = new string[array.Length];
+    int count = 0;
+
+    for( int i = 0; i < array.Length; i++)
+    {
+        string input = array[i];
+
+        if (input.Length <= 3)
+        {
+            arr[count] = input;
+            count++;
+        }
+    }
+    return arr;
 }
 
